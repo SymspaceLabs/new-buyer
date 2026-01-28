19efd3a25c78556692d83f54dbf3c0d7f7ddc24f
Thilak Sundaram <techcrowdmy@gmail.com>
2026-01-28 14:29:25 +0800

    Initial Commit

--- app/(auth)/otp-forgot-password/page.tsx
+++ app/(auth)/otp-forgot-password/page.tsx
@@ -118,7 +118,6 @@ export default function OtpForgotPassword() {
         
         <SymSubmitButton
           isValid={!(cooldown > 0) && !loading}
-          disabled={cooldown > 0 || loading}
           onClick={handleResendCode}
           loading={loading}
         >
